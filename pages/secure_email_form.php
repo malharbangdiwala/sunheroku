9c2cffc32dc9f2a274973c0190e0a0f758a2ce5f
malharbangdiwala <malharbangdiwala03@gmail.com>
2021-09-03 17:26:18 +0530

    1234567

--- pages/secure_email_form.php
+++ pages/secure_email_form.php
@@ -110,7 +110,23 @@
             <input type="reset" name="reset" value="Reset Form">
           </div>
         </form>
-        <h3><?php include "secure_email_code.php"?></h3>
+        <div id="feedback">
+<!-- Heading Of The Form -->
+<div class="head">
+<h3>FeedBack Form</h3>
+<p>This is feedback form. Send us your feedback !</p>
+</div>
+<!-- Feedback Form -->
+<form action="#" id="form" method="post" name="form">
+<input name="vname" placeholder="Your Name" type="text" value="">
+<input name="vemail" placeholder="Your Email" type="text" value="">
+<input name="sub" placeholder="Subject" type="text" value="">
+<label>Your Suggestion/Feedback</label>
+<textarea name="msg" placeholder="Type your text here..."></textarea>
+<input id="send" name="submit" type="submit" value="Send Feedback">
+</form>
+<h3><?php include "secure_email_code.php"?></h3>
+</div>
       </div>
       <div id= "map" class="one_half">
       <h1>Address</h1>
