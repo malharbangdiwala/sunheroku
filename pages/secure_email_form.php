4ab6c497373d767e3e4bdaef8f3ac289f6c68f90
malharbangdiwala <malharbangdiwala03@gmail.com>
2021-09-03 17:46:16 +0530

    123456789

--- pages/secure_email_form.php
+++ pages/secure_email_form.php
@@ -1,27 +1,3 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "malharbangdiwala03@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -116,12 +92,16 @@ if(isset($_POST['submit'])){
     <div class="content"> 
       <!-- ################################################################################################ -->
 <!-- Feedback Form Starts Here -->
-<form action="" method="post">
-First Name: <input type="text" name="first_name"><br>
-Last Name: <input type="text" name="last_name"><br>
-Email: <input type="text" name="email"><br>
-Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
-<input type="submit" name="submit" value="Submit">
+<form action="secure_email_code.php" method="post">
+
+<label>Email Address</label>
+<input type="text" name="email_address" size="40">
+
+<label>Your Feedback</label>
+<textarea name="feedback" cols="50" rows="10"></textarea>
+
+<input type="submit" name="send" value="Submit">
+
 </form>
 <!-- Feedback Form Ends Here -->
 </div>
