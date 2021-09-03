5669a2c12eb527182c17a41ce30bd1333ec626bd
malharbangdiwala <malharbangdiwala03@gmail.com>
2021-09-03 18:08:29 +0530

    12

--- secure_email_form.php
+++ secure_email_form.php
@@ -6,12 +6,13 @@
 <body>
 
 <form action="feedback_form.php" method="post">
-
+<label>Name</label>
+<input type="text" name="name" size="40">
 <label>Email Address</label>
-<input type="text" name="email_address" size="40">
+<input type="text" name="email" size="40">
 
 <label>Your Feedback</label>
-<textarea name="feedback" cols="50" rows="10"></textarea>
+<textarea name="comment" cols="50" rows="10"></textarea>
 
 <input type="submit" name="send" value="Submit">
 
